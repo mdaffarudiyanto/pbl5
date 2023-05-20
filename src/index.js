3c46fdb24946823bfbdf672f489c4b30a621f6c9
mdaffarudiyanto <72158400+mdaffarudiyanto@users.noreply.github.com>
2023-05-21 00:35:27 +0900

    Version 10

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'true', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+// const cors = require('cors');
+// const corsOptions ={
+//     origin:'http://localhost:3001', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+
+const items = [];
+exports.getItems = functions.region('us-central1').https.onRequest((request, response) => {
+    // CORS用にAccess-Control-Allow系ヘッダを追加
+    response.set('Access-Control-Allow-Origin', 'https://mdaffarudiyanto.github.io/'); // localhostを許可
+    response.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST'); // DELETEだけは拒否
+    response.set('Access-Control-Allow-Headers', 'Content-Type'); // Content-Typeのみを許可
+
+    response.json({ items });
+});
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,7 +31,7 @@ root.render(
   
 );
 
-App.use(cors(corsOptions));
+// App.use(cors(corsOptions));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
