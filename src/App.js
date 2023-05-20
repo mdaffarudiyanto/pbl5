3e801cf7842d3f47e0151bf93456018e5a2685a6
mdaffarudiyanto <72158400+mdaffarudiyanto@users.noreply.github.com>
2023-05-20 22:02:57 +0900

    Version 8

--- src/App.js
+++ src/App.js
@@ -7,28 +7,28 @@ import { json } from "./json";
 
 
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBV6vRKz2CuPx9GlNnGD89FiRS-yH7eGys",
-//   authDomain: "pbl5-mdr.firebaseapp.com",
-//   databaseURL: "https://pbl5-mdr-default-rtdb.firebaseio.com",
-//   projectId: "pbl5-mdr",
-//   storageBucket: "pbl5-mdr.appspot.com",
-//   messagingSenderId: "163531844929",
-//   appId: "1:163531844929:web:c02b60ff032930f0b8c5c0",
-//   measurementId: "G-6645BR6Q6N"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBV6vRKz2CuPx9GlNnGD89FiRS-yH7eGys",
+  authDomain: "pbl5-mdr.firebaseapp.com",
+  databaseURL: "https://pbl5-mdr-default-rtdb.firebaseio.com",
+  projectId: "pbl5-mdr",
+  storageBucket: "pbl5-mdr.appspot.com",
+  messagingSenderId: "163531844929",
+  appId: "1:163531844929:web:c02b60ff032930f0b8c5c0",
+  measurementId: "G-6645BR6Q6N"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function SurveyComponent() {
   const survey = new Model(json);
