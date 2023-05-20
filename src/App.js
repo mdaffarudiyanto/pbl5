a9dcdbc3b6daec694ff4b608b43399be42dd26cd
mdaffarudiyanto <72158400+mdaffarudiyanto@users.noreply.github.com>
2023-05-20 19:01:59 +0900

    Version 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+import "./index.css";
+import { json } from "./json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function SurveyComponent() {
+  const survey = new Model(json);
+  
+  survey.onComplete.add(function (sender, options) {
+    // Display the "Saving..." message (pass a string value to display a custom message)
+    options.showSaveInProgress();
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://pbl5-mdr-default-rtdb.firebaseio.com/survey");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        // Display the "Success" message (pass a string value to display a custom message)
+        options.showSaveSuccess();
+        // Alternatively, you can clear all messages:
+        // options.clearSaveMessages();
+      } else {
+        // Display the "Error" message (pass a string value to display a custom message)
+        options.showSaveError();
+      }
+    };
+    xhr.onerror = function () {
+      // Display the "Error" message (pass a string value to display a custom message)
+      options.showSaveError();
+    };
+    xhr.send(JSON.stringify(sender.data));
+  });
+
+  return <Survey model={survey} />;
 }
 
-export default App;
+export default SurveyComponent;
